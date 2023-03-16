1100cded1360ad25de62610cd403f2b100967e68
DavidTrindade98 <dtrindade1998@gmail.com>
2023-03-16 21:01:57 +0000

    starting project environment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import logo from '../assets/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='first-page'>
+        <img src={logo} id="logo" alt='logo' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
