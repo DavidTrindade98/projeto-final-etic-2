acd018dbb3fb03c38ad56c52128fb406fef17739
DavidTrindade98 <dtrindade1998@gmail.com>
2023-04-04 03:39:58 +0100

    First Page root to Second Page and some css

--- src/pages/welcome/FirstPage.js
+++ src/pages/welcome/FirstPage.js
@@ -1,17 +1,25 @@
-import '../../styles/welcome/welcome.css';
-import logo from '../../assets/logo.svg';
+import "../../styles/welcome/FirstPage.css";
+import logo from "../../assets/logo.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function FirstPage() {
+  const navigate = useNavigate();
+
   return (
-      <div className='first-page'>
-        <div className='logo-slogan'>
-            <img src={logo} id="logo" alt='logo' />
+    <button id="screen-button" onClick={() => navigate("/SignupOrLogin")}>
+      <default-screen>
+        <div className="first-page">
+          <div className="logo-slogan">
+            <img src={logo} id="logo" alt="logo" />
             <text>Live it like the Locals</text>
+          </div>
+          <div className="tap-anywhere">
+            <text>
+              Tap anywhere to<br></br>continue
+            </text>
+          </div>
         </div>
-      <div className='tap-anywhere'>
-        <text>Tap anywhere to<br></br>continue</text>
-      </div>
-      </div>
+      </default-screen>
+    </button>
   );
 }
-
--- src/pages/welcome/SignupORlogin.js
+++ src/pages/welcome/SignupORlogin.js
@@ -1,9 +1,16 @@
-import '../../styles/welcome/welcome.css'
+import "../../styles/welcome/SignupORlogin.css";
+import "../../styles/App.css";
+import logo from "../../assets/logo.svg";
 
-export default function SignupORlogin(){
-    return(
-        <div className='welcome2'>
-            <p>Hello</p>
-        </div>
-    );
+export default function SignupORloginPage() {
+  return (
+    <default-screen>
+      <div className="SignupORloginPage">
+        <img src={logo} id="logo-signup" alt="logo" />
+        <text id="hey-have-we-met">
+          Hey,<br></br>have we met?
+        </text>
+      </div>
+    </default-screen>
+  );
 }
