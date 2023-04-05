3bf2353b68f8f750e735e5083806f0e1a4037950
DavidTrindade98 <dtrindade1998@gmail.com>
2023-04-05 04:07:19 +0100

    Doing Page 3 Animation

--- src/pages/signup/SignupPage.js
+++ src/pages/signup/SignupPage.js
@@ -2,13 +2,15 @@ import "../../styles/signup/SignupPage.css";
 import logo from "../../assets/logo.svg";
 import "../../styles/App.css";
 
-
 export default function SignupPage() {
   return (
-    <default-view>
+    <default-screen>
       <div className="SignupPage">
         <img src={logo} id="logo-signup-page" alt="logo" />
+        <div className="sign-up-page-text">
+          
+        </div>
       </div>
-    </default-view>
+    </default-screen>
   );
 }
--- src/pages/welcome/SignupORlogin.js
+++ src/pages/welcome/SignupORlogin.js
@@ -1,29 +1,37 @@
+import { useState, useRef } from "react";
 import "../../styles/welcome/SignupORlogin.css";
 import "../../styles/App.css";
 import logo from "../../assets/logo.svg";
-import { useNavigate } from "react-router-dom";
 
 export default function SignupORloginPage() {
 
-  const navigate = useNavigate();
+  const [show, setShow] = useState(true);
+  const ref = useRef(null);
 
   return (
     <default-screen>
       <div className="SignupORloginPage">
         <img src={logo} id="logo-signup" alt="logo" />
         <div className="text-signup-page">
-        <text id="hey-have-we-met">
-          Hey,<br></br>have we met?
-        </text>
-        <text id="signup-to-start">
+          <text id="hey-have-we-met" ref={ref}>
+            Hey,<br></br>have we met?
+          </text>
+          <text id="signup-to-start">
             Sign up to start or login to your account.
-        </text>
-        <button className="signup-button-overlay" onClick={() => navigate("/Signup")} >
-          <text id="signup-text">Sign up</text>
-        </button>
-        <text id="login-text-button">Log in</text>
+          </text>
+          <button
+            className="signup-button-overlay"
+            onClick={() => {
+              setShow(!show);
+            }}
+          >
+            <text id="signup-text">Sign up</text>
+          </button>
+          <text id="login-text-button">Log in</text>
         </div>
+        {!show && <p ref={ref} id="hey-have-we-met">Sign up<br></br> with e-mail</p>}
       </div>
     </default-screen>
+    
   );
 }
