28d72b160950cbb6367d59b285d3ca40a9a6683b
DavidTrindade98 <dtrindade1998@gmail.com>
2023-04-05 22:32:35 +0100

    Third Page Animations Ongoing

--- src/pages/welcome/SignupORlogin.js
+++ src/pages/welcome/SignupORlogin.js
@@ -2,35 +2,54 @@ import "../../styles/welcome/SignupORlogin.css";
 import "../../styles/App.css";
 import logo from "../../assets/logo.svg";
 import { useState } from "react";
-
+import back from "../../assets/back.svg";
 
 export default function SignupORloginPage() {
   
-  const[isClicked, notClicked] = useState(true);
+  const [isClicked, notClicked] = useState(true);
 
-  function Clicked(){
+  function Clicked() {
     notClicked(!isClicked);
     console.log("Clicked");
-  } 
+  }
 
   return (
     <default-screen>
       <div className="SignupORloginPage">
+        <img
+          src={back}
+          id={isClicked ? "logo-back-hide" : "logo-back-show"} alt="logo-back"
+          onClick={Clicked}
+        />
         <img src={logo} id="logo-signup" alt="logo" />
         <div className="text-signup-page">
-          <text id="hey-have-we-met">
-            Hey,<br></br>have we met?
+          <text id={isClicked ? "hey-have-we-met" : "hey-have-we-met-2"}>
+            <span>
+              Hey,<br></br>have we met?
+            </span>
           </text>
-          <text id={isClicked ? "signup-to-start" : "noDisplayText"}>
+          <text
+            id={isClicked ? "signup-to-start" : "signup-to-start-no-display"}
+          >
             Sign up to start or login to your account.
           </text>
           <button
-            className="signup-button-overlay"
-            onClick={() => {Clicked()}}
+            className={
+              isClicked ? "signup-button-overlay" : "signup-button-overlay-2"
+            }
+            onClick={() => {
+              Clicked();
+            }}
           >
             <text id="signup-text">Sign up</text>
           </button>
-          <text id="login-text-button">Log in</text>
+          <text
+            id={
+              isClicked ? "login-text-button" : "login-text-button-no-display"
+            }
+          >
+            Log in
+          </text>
         </div>
       </div>
     </default-screen>
