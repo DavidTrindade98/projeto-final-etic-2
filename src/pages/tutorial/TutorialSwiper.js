18c9df6998d0c29189242d45879ed8c1fc27efc1
DavidTrindade98 <dtrindade1998@gmail.com>
2023-04-17 21:39:30 +0100

    Doing Tutorial

--- src/pages/tutorial/TutorialSwiper.js
+++ src/pages/tutorial/TutorialSwiper.js
@@ -1,26 +1,38 @@
 import "../../styles/tutorial/tutorial_swiper.css";
 import LogoSmall from "../../components/LogoSmall.js";
 import People from "../../assets/people.svg";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function TutorialSwiper() {
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+export default function TutorialSwiper(){
   return (
     <default-screen>
-        <LogoSmall />
-        <div className="tutorialOne">
-        <p id="hi_tutorial_text">
-          Hi, Jane!<br></br>Here's a short<br></br>tutorial for you:
-        </p>
-        <img src={People} id="people_icon" alt="logo" />
-        <div className="hi_tutorial_text_2_container">
-          <p id="hi_tutorial_text_2">
-          Dakee was created to give the real,<br></br> non-touristic experience in a
-          visited city.<br></br><br></br> Based on your interests, we connect Travellers and
-          Locals to exchange ideas<br></br> and give tips on what is actually worth<br></br> to do
-          and see. No tourist cliché!
-        </p>
-        </div>
-        
-      </div>
+    <Swiper
+    
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide>
+      <LogoSmall />
+        <div className="tutorial">
+        <img src={People} alt="people" id="people_icon"></img></div>
+        </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+    </Swiper>
     </default-screen>
   );
-}
+};
