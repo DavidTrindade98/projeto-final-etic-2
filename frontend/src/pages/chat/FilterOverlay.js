3623b9be97b0bceef6b9439d28edd1adbe8c7b18
Inna Klymovych <98186099+InnaKlymovych@users.noreply.github.com>
2023-05-04 20:50:03 +0100

    Filter page almost done

--- frontend/src/pages/chat/FilterOverlay.js
+++ frontend/src/pages/chat/FilterOverlay.js
@@ -1,6 +1,10 @@
 import "../../styles/chat/filter_overlay.css";
 import filter from "../../assets/filter.svg";
 import "../../styles/chat/locals_search.css";
+import art from "../../assets/art.svg";
+import food from "../../assets/food.svg";
+import music from "../../assets/music.svg";
+import VectorStar from "../../assets/VectorStar.svg";
 
 export default function FilterMenu() {
 
@@ -27,6 +31,33 @@ export default function FilterMenu() {
               <p>40</p>
               <p>+60</p>
             </div>
+          </div>
+        </div>
+        <div className="advices-in-container">
+          <h2>Advices in</h2>
+          <div className="advices-in-container-gap">
+            <div className="filter-advice-choice">
+              <img src={art} alt="art" ></img>
+              <p>art</p>
+            </div>
+            <div className="filter-advice-choice">
+              <img src={food} alt="food" ></img>
+              <p>food</p>
+            </div>
+            <div className="filter-advice-choice">
+              <img src={music} alt="music" ></img>
+              <p>music</p>
+            </div>
+          </div>
+        </div>
+        <div className="filter-minimal-rating-container">
+          <h2>Minimal Rating</h2>
+          <div className="filter-stars-container">
+              <img src={VectorStar} alt="star"></img>
+              <img src={VectorStar} alt="star"></img>
+              <img src={VectorStar} alt="star"></img>
+              <img src={VectorStar} alt="star"></img>
+              <img src={VectorStar} alt="star"></img>
           </div>
         </div>
       </div>
