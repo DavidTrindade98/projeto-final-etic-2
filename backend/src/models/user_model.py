18e788d32539f633d158d3d1d9b52b67d87749d0
Inna Klymovych <98186099+InnaKlymovych@users.noreply.github.com>
2023-05-23 20:56:56 +0100

    Doing User Profile Models

--- backend/src/models/user_model.py
+++ backend/src/models/user_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, Integer, ForeignKey
 from sqlalchemy import create_engine
 from src.config import Settings
 
@@ -12,5 +12,29 @@ class User(BaseDatabaseModel):
     email = Column(String(255), primary_key=True, index=True)
     name = Column(String(255))
     hashed_password = Column(String(255))
+
+    age = Column(String(255))
+    gender = Column(String(255))
+    live_in = Column(String(255))
+
+class UserCities(BaseDatabaseModel):
+    __tablename__ = "user_cities"
+    user_email = Column(String(255), ForeignKey("users.email"), primary_key=True)
+    city = Column(String(255),primary_key=True)
+
+class UserExperiences(BaseDatabaseModel):
+    __tablename__ = "user_experiences"
+    user_email = Column(String(255), ForeignKey("users.email"),primary_key=True)
+    experiences = Column(String(255),primary_key=True)
+
+class Cities(BaseDatabaseModel):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+
+class Experiences(BaseDatabaseModel):
+    __tablename__ = "experiences"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
 
 BaseDatabaseModel.metadata.create_all(engine)
